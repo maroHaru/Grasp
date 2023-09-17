3c34b1d7cf610f9a882c404957a4e8625602815a
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-09-17 14:46:50 +0000

    [Add] git add . git commit -m [Add]

--- app/controllers/worker/jobs_controller.rb
+++ app/controllers/worker/jobs_controller.rb
@@ -1,16 +1,33 @@
 class Worker::JobsController < ApplicationController
-  
-  def index
-    @jobs = Job.all
-    @job = Job.new
-  end
-  
+
   def create
     @job = Job.new(job_params)
+    @job.worker_id = current_worker.id
     @job.save
     redirect_to jobs_path
   end
-  
+
+  def index
+    @jobs = current_worker.jobs
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params
