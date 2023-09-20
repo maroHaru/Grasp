4f27705540e036546d8edcfcef68d30955134713
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-09-20 11:52:42 +0000

    [Add] Implemented completed status changes

--- app/controllers/worker/to_dos_controller.rb
+++ app/controllers/worker/to_dos_controller.rb
@@ -2,20 +2,23 @@ class Worker::ToDosController < ApplicationController
 
   def index
     @to_do = ToDo.new
-    @to_dos = ToDo.where(is_completed: "false")
-    @complete_to_dos = ToDo.where(is_completed: "true")
+    @to_dos = ToDo.where(is_completed: false)
+    @complete_to_dos = ToDo.where(is_completed: true)
     # @to_dos = ToDo.all (←未完了も完了も全て表示されてしまう。。)
     # ToDo.where(is_completed: "false") ←完了ステータスがFALSEのものだけ表示する事が可能
   end
 
   def create
-    @to_do = ToDo.new(to_do_params)
-    @to_do.worker_id = current_worker.id
-    @to_do.save
+    to_do = ToDo.new(to_do_params)
+    to_do.worker_id = current_worker.id
+    to_do.save
     redirect_to to_dos_path
   end
 
   def update
+    to_do = ToDo.find(params[:id])
+    to_do.update(to_do_params)
+    redirect_to to_dos_path
 
   end
 
@@ -23,9 +26,11 @@ class Worker::ToDosController < ApplicationController
   end
 
   def complete
-    @to_do = ToDo.find(params[:id])
+    today = Date.today
+    to_do = ToDo.find(params[:to_do][:to_do_id])
+    to_do.update(completed_date: today, is_completed: true)
     # @to_do.worker_id = current_worker.id
-    @to_do.update(is_completed: true)
+    # @to_do.update(is_completed: true)
     redirect_to to_dos_path
   end
 
