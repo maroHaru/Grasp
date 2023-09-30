2773b1532c23fd12b3a788598264ca2739dc6673
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-09-30 09:02:35 +0000

    [Add] Implement time calculation

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -4,4 +4,8 @@ class Timecard < ApplicationRecord
   belongs_to :client, optional: true
   belongs_to :job, optional: true
   belongs_to :to_do, optional: true
+
+  def amount_of_time
+    Time.at(end_time - start_time).utc.strftime('%R')
+  end
 end
