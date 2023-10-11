1bc0f8a48d470ed459b70085fb6c06e88f3004b6
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-10-11 16:55:54 +0000

    [Add] Creating a list of Timecard hours on the Worker screen

--- app/controllers/worker/homes_controller.rb
+++ app/controllers/worker/homes_controller.rb
@@ -1,9 +1,11 @@
 class Worker::HomesController < ApplicationController
   def top
-    @to_do = ToDo.new
-    @to_dos = current_worker.to_dos.where(is_completed: false)
+
   end
 
   def about
+    @to_do = ToDo.new
+    @to_dos = current_worker.to_dos.where(is_completed: false)
+    @timecards = current_worker.timecards
   end
 end
