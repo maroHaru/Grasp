132ce6a832070c29c0b9784fbe9f9b6c94ea8ab5
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-10-12 12:40:36 +0000

    [Update] Organizing the about page

--- app/controllers/worker/homes_controller.rb
+++ app/controllers/worker/homes_controller.rb
@@ -4,8 +4,6 @@ class Worker::HomesController < ApplicationController
   end
 
   def about
-    @to_do = ToDo.new
-    @to_dos = current_worker.to_dos.where(is_completed: false)
     @timecards = current_worker.timecards
   end
 end
