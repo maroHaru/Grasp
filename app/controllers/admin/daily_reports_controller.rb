77d5d9660721b8e75e0685c4784d49ed7ca7acbb
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-10-18 08:50:39 +0000

    [Update] Display total time in Worker timecard list on admin screen

--- app/controllers/admin/daily_reports_controller.rb
+++ app/controllers/admin/daily_reports_controller.rb
@@ -14,15 +14,15 @@ class Admin::DailyReportsController < ApplicationController
     @timecards = @daily_report.timecards
     @comment = Comment.new
     # @worker = @daily_reports.worker
-    # total = []
-    # @daily_report.timecards.each do |card|
-    # # @daily_report = card.daily_report
-    #   if card.end_time == nil || card.start_time == nil
-    #     next
-    #   end
-    #   diff = card.end_time - card.start_time
-    #   total << diff
-    # end
-    #   @total = Time.at(total.sum).utc.strftime('%R')
+    total = []
+    @daily_report.timecards.each do |card|
+    # @daily_report = card.daily_report
+      if card.end_time == nil || card.start_time == nil
+        next
+      end
+      diff = card.end_time - card.start_time
+      total << diff
+    end
+      @total = Time.at(total.sum).utc.strftime('%R')
   end
 end
