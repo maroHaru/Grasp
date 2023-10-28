6c3949a55ef78f4960831c60a8590dcd1108348c
maroHaru <134841618+maroHaru@users.noreply.github.com>
2023-10-28 07:54:44 +0000

    [Update] Implementation of notification function

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,2 +1,10 @@
 class Admin::NotificationsController < ApplicationController
+
+  def index
+    # @notifications = Notification.all
+    @notifications = Notification.where(visiter_id: current_admin.id).where(is_checked: false).order(created_at: :desc)
+  end
+
+  def update
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,9 +1,8 @@
 class Notification < ApplicationRecord
-  belongs_to :visiter, class_name: 'Admin', foreign_key: 'visiter_id'
-  belongs_to :visited, class_name: 'Admin', foreign_key: 'visited_id'
-  belongs_to :visiter, class_name: 'Worker', foreign_key: 'visiter_id'
-  belongs_to :visited, class_name: 'Worker', foreign_key: 'visited_id'
-  belongs_to :notifiable, polymorphic: true
+  # belongs_to :visiter, class_name: 'Admin', foreign_key: 'visiter_id'
+  # belongs_to :visited, class_name: 'Admin', foreign_key: 'visited_id'
+  # belongs_to :visiter, class_name: 'Worker', foreign_key: 'visiter_id'
+  # belongs_to :visited, class_name: 'Worker', foreign_key: 'visited_id', optional: true
 
   enum action: { comment: 0, checked_item: 1 }
 end
